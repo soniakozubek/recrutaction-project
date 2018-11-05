c372b9e9308419bf571696a36a656a4d8b82df8b
soniakozubek <sonia.kozubek@gmail.com>
2018-11-05 23:21:14 +0100

    Update page-object.js

--- page-object.js
+++ page-object.js
@@ -1,10 +1,10 @@
 let pageObject = function () {
 
     this.url = 'https://qarecruitment.egnyte.com/fl/WFFhkIrOdZ#folder-link';
-    this.password = 'VmagXuHh'
+    this.password = 'VmagXuHh';
     this.passwordField = $('#password');
     this.continueButton = element(by.linkText('Continue'));
-    this.listOfFilesFromFolder = $$('.file-info')
+    this.listOfFilesFromFolder = $$('.file-info');
 
     this.openLoginPage = () => {
         browser.waitForAngularEnabled(false);
@@ -15,50 +15,56 @@ let pageObject = function () {
         browser.get('https://qarecruitment.egnyte.com/fl/WFFhkIrOdZ#folder-link/Sonia%20Kozubek');
         browser.waitForAngular();
     };
+    
     this.openNotExistingFolder = () => {
         browser.get('https://qarecruitment.egnyte.com/fl/WFFhkIrOdZ#folder-link/Sonia%20Kozubekk/folderA/folderZ');
         browser.waitForAngular();
     };
+    
     this.enterPassword = () => {
-        this.passwordField.sendKeys(this.password)
-        this.continueButton.click()
-    }
+        this.passwordField.sendKeys(this.password);
+        this.continueButton.click();
+    };
+    
     this.login = () => {
         this.openLoginPage();
         this.enterPassword();
         browser.waitForAngular();
-    }
+    };
 
     this.navigateTo = ($folderName) => {
-        let element = $('span[title="' + $folderName + '"]')
-        browser.wait(protractor.ExpectedConditions.presenceOf(element), 5000)
-            element.click()
-            browser.sleep(1000)
+        let element = $('span[title="' + $folderName + '"]');
+        browser.wait(protractor.ExpectedConditions.presenceOf(element), 5000);
+            element.click();
+            browser.sleep(1000);
             browser.waitForAngular();
-    }
+    };
 
     this.getCurrentFolderName = () => {
-        return $('.crumb.crumb-current').getText()
-
-    }
+        return $('.crumb.crumb-current').getText();
+    };
+    
     this.checkIfFilesAreDisplayed = () => {
-        return $$('.file-info').isPresent()
-    }
+        return $$('.file-info').isPresent();
+    };
 
     this.checkIfImagesInGalleryViewAreDisplayed = () => {
-        return $$('.gallery-item.ui-selectee').isPresent()
-    }
+        return $$('.gallery-item.ui-selectee').isPresent();
+    };
+    
     this.countFilesInFolder = () => {
-        return $$('.file-info').count()
-    }
+        return $$('.file-info').count();
+    };
+    
     this.countImagesInGalleryView = () => {
-        return $$('.gallery-item.ui-selectee').count()
-    }
+        return $$('.gallery-item.ui-selectee').count();
+    };
+    
     this.changeSortingOrder = ($order) => {
-        $$('.dropdown-toggle').click()
-        $$('li[data-order="' + $order + '"]').click()
-        browser.sleep(1000)
-    }
+        $$('.dropdown-toggle').click();
+        $$('li[data-order="' + $order + '"]').click();
+        browser.sleep(1000);
+    };
 
     this.getFilesNames = () => {
         let listOfFiles = []
@@ -70,7 +76,7 @@ let pageObject = function () {
         }).then(() =>
             listOfFiles
         )
-    }
+    };
 
     this.checkIfFilesAreSortedByNameDesc = async () => {
         await this.getFilesNames()
@@ -84,22 +90,22 @@ let pageObject = function () {
                 this.getFilesNames().then(($list) => {
                     expect($list).toEqual(listofSortedNames);
                 })
-            })
-    }
+            });
+    };
 
     this.turnGalleryView = () => {
-        $$('button[data-mode="gallery"]').click()
-        browser.sleep(1000)
-    }
+        $$('button[data-mode="gallery"]').click();
+        browser.sleep(1000);
+    };
 
     this.getListViewButton = () => {
-        return $$('button[data-mode="list"]')
-    }
+        return $$('button[data-mode="list"]');
+    };
 
     this.getURLError = () => {
         let element = $$('.span9.description');
-        browser.wait(protractor.ExpectedConditions.presenceOf(element), 10000)
-        return element.getText()  
+        browser.wait(protractor.ExpectedConditions.presenceOf(element), 10000);
+        return element.getText();  
     }
 };
 module.exports = pageObject;
